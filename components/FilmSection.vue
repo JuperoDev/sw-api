a82fd62145833d170553c8b02fe10bda89de27c8
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-06-24 23:11:33 +0200

    feature: film render  as a component

---
+++ components/FilmSection.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="films.length">
+      <h2>Films</h2>
+      <div v-for="film in films" :key="film.url">
+        <p>{{ film.title }}</p>
+        <NuxtLink :to="getFilmLink(film.url)">
+          <v-btn color="primary">View Film</v-btn>
+        </NuxtLink>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  
+  interface Film {
+    title: string;
+    url: string;
+  }
+  
+  const props = defineProps<{ films: Film[] }>()
+  
+  const getFilmLink = (url: string): string => {
+    const id = url.split('/').filter(Boolean).pop()
+    return `/films/${id}`
+  }
+  </script>
+  

fe09a98899dfde6b39c674eb825f8cfdfd626ded
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-06-25 00:01:36 +0200

    feature/chore: autocomplete is now a component

---
+++ components/CharacterAutocomplete.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="p-4">
+      <v-autocomplete
+        v-model="search"
+        :items="characterNames"
+        :loading="loading"
+        hide-no-data
+        hide-selected
+        label="Search for a character"
+        @change="onCharacterSelect"
+      ></v-autocomplete>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, computed, onMounted, watch } from 'vue'
+  import { useRouter } from 'vue-router'
+  import { useCharactersStore } from '~/stores/characters'
+  
+  const search = ref('')
+  const router = useRouter()
+  const charactersStore = useCharactersStore()
+  
+  onMounted(() => {
+    if (charactersStore.characters.length === 0) {
+      charactersStore.fetchCharacters()
+    }
+  })
+  
+  const loading = computed(() => charactersStore.loading)
+  const characterNames = computed(() =>
+    charactersStore.characters.map(character => character.name)
+  )
+  
+  const onCharacterSelect = (selectedName: string) => {
+    const selectedCharacter = charactersStore.characters.find(character => character.name === selectedName)
+    if (selectedCharacter) {
+      const id = selectedCharacter.url.split('/').filter(Boolean).pop()
+      const targetUrl = `/people/${id}`
+      router.push(targetUrl)
+    }
+  }
+  
+  watch(search, (newValue) => {
+    onCharacterSelect(newValue)
+  })
+  </script>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,5 @@
 <template>
-  <div class="p-4">
-    <v-autocomplete
-      v-model="search"
-      :items="characterNames"
-      :loading="loading"
-      hide-no-data
-      hide-selected
-      label="Search for a character"
-    ></v-autocomplete>
+  <div>
+    <CharacterAutocomplete />
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref, computed, onMounted, watch } from 'vue'
-import { useRouter } from 'vue-router'
-import { useCharactersStore } from '~/stores/characters'
-
-const search = ref('')
-const router = useRouter()
-const charactersStore = useCharactersStore()
-
-onMounted(() => {
-  if (charactersStore.characters.length === 0) {
-    charactersStore.fetchCharacters()
-  }
-})
-
-const loading = computed(() => charactersStore.loading)
-const characterNames = computed(() =>
-  charactersStore.characters.map(character => character.name)
-)
-
-watch(search, (newValue) => {
-  const selectedCharacter = charactersStore.characters.find(character => character.name === newValue)
-  if (selectedCharacter) {
-    const id = selectedCharacter.url.split('/').filter(Boolean).pop()
-    const targetUrl = `/people/${id}`
-    router.push(targetUrl)
-  }
-})
-</script>

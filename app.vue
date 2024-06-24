5ad2ef8f3817e50a934e5ae1b6a3fec8dfabc4d2
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-06-24 16:17:34 +0200

    install: vuetify & icons

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-sdf
-<v-btn>Hola</v-btn>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
+    <NuxtPage />
   </div>
 </template>
-
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   modules:[
     '@pinia/nuxt'
   ],
-  css: ['vuetify/lib/styles/main.sass','~/assets/css/main.css'],
+  css: ['vuetify/lib/styles/main.sass','~/assets/css/main.css','@mdi/font/css/materialdesignicons.min.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
